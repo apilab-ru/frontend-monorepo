a46b87b6cb23bb4d93c69a98c75d8d3169119d93
Svetlana <sss041296@mail.ru>
2021-10-09 15:56:32 +0400

    add img, colors.scss, page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './pages/page/page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AboutComponent } from './components/about/about.component';
+import { TitleComponent } from './components/title/title.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SvgZigzagComponent } from './components/svg-zigzag/svg-zigzag.component';
+import { ItemWorkComponent } from './components/item-work/item-work.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { ItemFeedbackComponent } from './components/item-feedback/item-feedback.component';
+import { ModalProjectComponent } from './components/modal-project/modal-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    HeaderComponent,
+    MenuComponent,
+    AboutComponent,
+    TitleComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    PortfolioComponent,
+    FeedbackComponent,
+    FooterComponent,
+    SvgZigzagComponent,
+    ItemWorkComponent,
+    ProjectCardComponent,
+    ItemFeedbackComponent,
+    ModalProjectComponent
   ],
   imports: [
     BrowserModule
